b800b5367cdcf5ea310a784ef35895819e2375b3
usama1234 <136082078+usamailyasbinmuhammadilyas@users.noreply.github.com>
2023-07-29 00:39:42 -0700

    my new counter

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let count= 0;
+
+function incre() {
+    count ++;
+    document.getElementById('first').innerHTML=count;
+}
+
+function reset() {
+    count =0;
+    document.getElementById("first").innerHTML=count;
+}
